75865a092865b0648d940797673b732467f835ab
sworc <lightbox@nifty.com>
2021-11-04 14:39:36 +0900

    Add files via upload

--- subject-1028-cs-post/php-test/db-select-02-heardoc.php
+++ subject-1028-cs-post/php-test/db-select-02-heardoc.php
@@ -11,7 +11,8 @@ $pdo = new PDO( $GLOBALS["connect_string"], $GLOBALS["user"], $GLOBALS["password
 // ***********************************
 // SQL 文字列
 // ***********************************
-$sql = "SELECT * FROM 社員マスタ WHERE 社員コード = :scode"; //>= :kyuyo ORDER BY 社員コード';
+// $sql = "SELECT * FROM 社員マスタ WHERE 社員コード = :scode"; //>= :kyuyo ORDER BY 社員コード';
+$sql = "UPDATE 社員マスタ set 氏名 = :sname WHERE 社員コード = :scode"; //>= :kyuyo ORDER BY 社員コード';
 // ***********************************
 // 準備
 // ***********************************
@@ -23,23 +24,29 @@ $statement = $pdo->prepare($sql);
 if ( !isset($_GET['scode']) ) {
     $_GET['scode'] = '0001';
 }
+if ( !isset($_GET['sname']) ) {
+    $_GET['sname'] = '匿名';
+}
 $scode = $_GET['scode'];
+$sname = $_GET['sname'];
 $statement->bindValue(':scode', $scode, PDO::PARAM_STR);
+$statement->bindValue(':sname', $sname, PDO::PARAM_STR);
 
 // ***********************************
 // 実行
 // ***********************************
-$statement->execute();
+$ret = $statement->execute();
 
 // ***********************************
 // 結果データを取得
 // ***********************************
-$html = "";
-$row = $statement->fetch(PDO::FETCH_ASSOC);
-if ( $row === false ) {
+// $row = $statement->fetch(PDO::FETCH_ASSOC);
+if ( $ret === false ) {
     print '{ "社員コード": "false"  }';
 }
 else {
+    $row["社員コード"] = $_GET['scode'];
+    $row["氏名"] = $_GET['sname'];
     print json_encode($row, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE );
 }
 
